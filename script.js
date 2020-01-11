a10bd9a62e50172e1717aac85629cc64d3bed2a7
Anna Kaplan <annakaplanart@gmail.com>
2020-01-11 18:16:48 -0500

    worked on red lady animation page?

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-const flipbook = require('./node_modules/flipbook/flipbook.min.js');
-flipbook({
-    id: 'animation',
-    path: 'animation/blueFaceActually',
-    filename: '%2d',
-    extension: 'jpg',
-    count: 78,
-    speed: 0.5
-});
+let height;
+let scrollStatus;
+const setScrollStatus = (status) => {
+    scrollStatus.innerHTML = status
+}
+window.onload = (e) => {
+    height = Number(getHeight().slice(0,-2));
+    scrollStatus = document.getElementById('scrollStatus');
+};
+window.onresize = (e) => {
+    height = getHeight();
+}
+window.onwheel = (e) => {
+    const currScroll = window.pageYOffset;
+    const scrollDown = e.deltaY > -1;
+    const halfWay = Number(height)/2
+    if (scrollDown) {
+        if (halfWay > currScroll) {
+            setScrollStatus('keep scrolling');
+        } else if ((halfWay * 1.5) < currScroll){
+            setScrollStatus('the end')
+        } else {
+            setScrollStatus('halfway there!')
+        }
+    } else {
+        if (currScroll === 0) {
+            setScrollStatus('back at the top');
+        } else {
+            setScrollStatus('going up!');
+        }
+    }
+}
+
+const getHeight = () => {
+    const animationStyle =  document.getElementById('animation').style;
+    const height =  Object.entries(animationStyle).find((el) => {
+        if (el[0] === 'height') {
+            return el;
+        };
+    });
+    document.getElementById('body').style = `height: ${height[1]};`;
+    return height[1];
+}
